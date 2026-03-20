66e4a7005ff84ca9980806067efbfa65ed4a7f46
krishna0018-coder <krishnaskb2004@gmail.com>
2026-03-20 16:35:18 +0530

    Added personal greeting program using Scanner

---
+++ Personal_greeting.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Personal-greeting {
+  public static void main(String[] agrs) {
+  Scanner sc = new Scanner(System.in);
+  System .out.println("Enter your name:");
+      String name = sc.nextLine();
+    System.out.println("Hello,"+name);
+  }// main end
+}// class end
